c0ebc2b0ea619e28d72d457fd5f72bacd531fc2a
Mor Oknin <morok@payoneer.com>
2020-10-05 19:20:47 +0300

    allign with latest version

--- GrafanaAnnotations/index.js
+++ GrafanaAnnotations/index.js
@@ -1,21 +1,39 @@
 const tl = require('vso-task-lib');
 const axios=require('axios');
+const process=require('process');
 
 var auth = tl.getEndpointAuthorization(tl.getInput("GrafanaService",true),true);
 var url = tl.getEndpointUrl(tl.getInput("GrafanaService",true),true);
 var tags = tl.getInput('Tags',true);
-var Annotation = tl.getInput("Annotation",true);
+var annotation = tl.getInput("Annotation",true);
 
 
+//remove spaces from tags 
+const tag = tags.replace(/\s/g, '');
+const config = { 
+    headers: { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${auth.parameters.apitoken}` 
+        } 
+    };
+const body = {
+    "time":Date.now(),
+    "tags":tag.split(","),
+    "text":`${annotation}`};
 
-const tag = tags.replace(/\s/g, '') 
-const config = { headers: { 'Content-Type': 'application/json', 'Accept': 'application/json',
-'Authorization': `Bearer ${auth.parameters.apitoken}` } };
-const body = {"time":Date.now(),"tags":tag.split(","),"text":`${Annotation}`};
 
-console.log(body)
+console.log("******** Parameters ********")
+console.log(`Grafana URL: ${url}`)
+console.log(`Grafana Tags: ` +tag.split(","))
+console.log(`Grafana Annotation: ${annotation}`)
+console.log("******** Parameters ********")
 
+console.log(`Crating Grafana Annotation in ${url}`)
 axios.post(`${url}api/annotations`,body, config)
 .then(r => r.data)
 .then(data=>console.log(data))
-.catch(error=>console.log(error));
+.catch(error=>{
+    console.log(error);
+    process.exit(1);
+});
--- README.md
+++ README.md
@@ -4,17 +4,43 @@ Summary
 Adding a Grafana annotations extensions for Azure Devops
 
 ## Release Notes
- 1.0.0
-     First release
+
+1.0.0
+	- First release
+1.0.10
+	- Release ready.
 
 ## What’s the problem
+
 When needing to add an annotations to Grafana once a release is finished. 
 
 ## Solution
-The Release task allow you to add a service connection with the following
- Endpoint - The Grafana URL ( will be concat later with apiAnnotations )
- APIkey - The Admin API key generated from Grafana
 
-and will get the following parametrs
- Tags - comma separated list of tags to send e.g (Deployments , $(yourVar) )
- Annotation - The Annotation text to send.
+The Release task allow you to add a service connection with the following
+
+* Endpoint - The Grafana URL ( will be concat later with api/Annotations )
+
+* APIkey - The Admin API key generated from Grafana
+
+## Usage
+
+Installs using the following:
+
+* A Grafana API endpoint with Token based authentication scheme for connecting securely to your Grafana server
+* A cross platform (Windows/Mac/Linux) build/release task for creating a tag in Grafana for any application.
+
+Steps:
+
+1. Create a Grafana API Endpoint:
+
+![Grafana API Endpoint](img/new_service.png)
+
+2. Open your build/release definition and add the Grafana task.
+
+![build/release](img/add_task.png)
+
+The task can be found in the Deploy category of the Add Tasks dialog. 
+Enter the required parameter values for the task:
+
+1. Tags- Mandatory Field
+2. Annotation- Mandatory Field
